f4ce0282b425a954cc6ee5bdca03529415b9e976
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2016-10-08 20:06:20 +0530

    update mad_doc_spider to download html documents

--- madurai/spiders/mad_doc_spider.py
+++ madurai/spiders/mad_doc_spider.py
@@ -1,15 +1,22 @@
 import scrapy
 from bs4 import BeautifulSoup
 
+import json
+import os
+
 class MadDocSpider(scrapy.Spider):
     name = 'mad_doc_spider'
     allowed_domains = [ 'projectmadurai.org' ]
     # read from file : index.json
-    #start_urls = 
-    root_url = 'http://www.projectmadurai.org/utf8/'
+    with open('index.json') as f:
+        index = json.load(f)
+    start_urls = [ 'http://www.projectmadurai.org/' + row['link'] for row in index ]
 
     def parse(self, response):
         # get soup from response
-        soup = BeautifulSoup(response.body,'lxml')
+        #soup = BeautifulSoup(response.body,'lxml')
         # read doc and yeild context indexed with link
-
+        url = response.url
+        # save html to file
+        with open('html/' + os.path.basename(url),'wb') as f:
+            f.write(response.body)
